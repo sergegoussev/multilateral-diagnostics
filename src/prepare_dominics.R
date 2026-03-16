8db2765a2542731dd2ec633733bda8b6731c0fc4
Serge Goussev <serge.goussev@statcan.gc.ca>
2026-03-16 09:10:01 -0400

    flush out more categories

--- src/prepare_dominics.R
+++ src/prepare_dominics.R
@@ -2,31 +2,38 @@
 source("src/dominicks_utils.R")
 
 #1. Download the data for each category
+
+#Beer
 download_category(
   category_name = "ber",
   output_path = "data/raw"
 )
 
+#Analgetics
 download_category(
   category_name = "ana",
   output_path = "data/raw"
 )
 
+#Fabric Softeners
 download_category(
   category_name = "fsf",
   output_path = "data/raw"
 )
 
+#Front-end-candies
 download_category(
   category_name = "fec",
   output_path = "data/raw"
 )
 
+#Cookies
 download_category(
   category_name = "coo",
   output_path = "data/raw"
 )
 
+#Bottled Juices
 download_category(
   category_name = "bjc",
   output_path = "data/raw"
